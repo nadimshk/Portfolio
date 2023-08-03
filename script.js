ca51d00096b4519e0a4e0cd6659a583fb984b77f
Nadeem Shaikh <wassup44@gmail.com>
2023-08-03 17:45:42 +0530

    profile pic, bg color, resume link update

--- script.js
+++ script.js
@@ -65,12 +65,12 @@ var resumeLink = document.querySelector("#resume-button-1");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1ps5mMt0B7RSdj5_TI2Lu0JGwc03yMSZ6/view?usp=sharing",
+    "https://drive.google.com/file/d/10GkYN2SF1vK3L5Ltpr_4fF_2q_vKSev_/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/uc?export=download&id=1ps5mMt0B7RSdj5_TI2Lu0JGwc03yMSZ6";
+    "https://drive.google.com/uc?export=download&id=10GkYN2SF1vK3L5Ltpr_4fF_2q_vKSev_";
   link.download = "Nadimuddin-Shaikh-Resume";
   link.click();
 });
@@ -79,12 +79,12 @@ var resumeLink = document.querySelector("#resume-button-2");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1ps5mMt0B7RSdj5_TI2Lu0JGwc03yMSZ6/view?usp=sharing",
+    "https://drive.google.com/file/d/10GkYN2SF1vK3L5Ltpr_4fF_2q_vKSev_/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/uc?export=download&id=1ps5mMt0B7RSdj5_TI2Lu0JGwc03yMSZ6";
+    "https://drive.google.com/uc?export=download&id=10GkYN2SF1vK3L5Ltpr_4fF_2q_vKSev_";
   link.download = "Nadimuddin-Shaikh-Resume";
   link.click();
 });
